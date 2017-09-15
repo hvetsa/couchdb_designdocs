a6faa43d011dadc8f0a50d3b2c8d8f0ebd9df7be
Hari Vetsa <hvetsa@us.ibm.com>
2017-09-15 19:24:44 +0000

    checkdb

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+
 exports.printMsg = function() {
   console.log("This is a message from the demo package");
-}
+};
+
+exports.checkDB = function ( couchURL, databaseName ) {
+	var couchDBURL = couchURL + "/" + databaseName;
+	return this.returnObjfromURL(couchDBURL);
+};
+
+
+exports.returnObjfromURL = function ( URL ) {
+
+    var syncRequest = require('sync-request');
+    var response = syncRequest('get', URL );
+    var responseString = response.getBody('utf8');
+    return JSON.parse(responseString);
+
+};
