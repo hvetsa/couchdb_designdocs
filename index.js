6fe3532778c9a9c458604175dba64d2028168154
Hari Vetsa <hvetsa@Haris-iMac.swg.usma.ibm.com>
2017-09-18 11:50:31 -0400

    enhanced http call

--- index.js
+++ index.js
@@ -5,15 +5,25 @@ exports.printMsg = function() {
 
 exports.checkDB = function ( couchURL, databaseName ) {
 	var couchDBURL = couchURL + "/" + databaseName;
-	return this.returnObjfromURL(couchDBURL);
+  return this.returnObjfromURL(couchDBURL);
 };
 
 
 exports.returnObjfromURL = function ( URL ) {
 
+    var returnObj = {}
     var syncRequest = require('sync-request');
+    
+    returnObj.startTime = (new Date).getTime();
     var response = syncRequest('get', URL );
-    var responseString = response.getBody('utf8');
-    return JSON.parse(responseString);
+    returnObj.endTime = (new Date).getTime();
 
+    returnObj.duration = returnObj.endTime - returnObj.startTime;
+    returnObj.duration = returnObj.duration + " milliseconds"
+
+    var responseString = response.getBody('utf8');
+
+    returnObj.responseCode = statusCode;
+    returnObj.responseString = responseString;
+    
 };
